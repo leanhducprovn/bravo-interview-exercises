8706ed7b67eef6bd6850286551799c182b54621c
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-01-08 23:37:35 +0700

    add table, page,...

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import product from '../../assets/data/product.json';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-product',
-  templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+    selector: 'app-product',
+    templateUrl: './product.component.html',
+    styleUrls: ['./product.component.css']
 })
 export class ProductComponent implements OnInit {
+    
+    listProduct: {
+        Id: string,
+        ParentId: string,
+        IsGroup: string,
+        Name: string,
+        ItemTypeName: string,
+        Code: string,
+        Unit: string,
+        ItemGroupCode: string,
+        BranchCode: string,
+        IsActive: string,
+        CreatedBy: string,
+        CreatedAt: string,
+        ModifiedBy: string,
+        ModifiedAt: string,
+        _SelectKey__wibkaw: string
+    }[] = product;
 
-  constructor() { }
+    check = true;
+    perPage = 20;
+    previous = 1;
+    next = 1;
+    page = 1;
 
-  ngOnInit(): void {
-  }
+    constructor(private route: ActivatedRoute,) { 
+        this.route.queryParams.subscribe(params => {
+            this.page = params['page'] ? params['page'] : 1;
+            this.previous = this.page - 1;
+            this.next = this.page - (-1);
+            this.check = this.page == 1 ? true : false;
+        });
+    }
+
+    ngOnInit(): void {
+
+    }
 
 }
