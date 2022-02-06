38bcb8f4e850b395a1f966140ba676caca430a6f
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-06 16:33:55 +0700

    update

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -10,38 +10,11 @@ export class ProductComponent implements OnInit {
     listProduct: {
         Id: string,
         ParentId: string,
-        IsGroup: string,
         Name: string,
         ItemTypeName: string,
         Code: string,
         Unit: string,
-        ItemGroupCode: string,
-        BranchCode: string,
-        IsActive: string,
-        CreatedBy: string,
-        CreatedAt: string,
-        ModifiedBy: string,
-        ModifiedAt: string,
-        _SelectKey__wibkaw: string
     }[] = product;
-
-    // Thêm sản phẩm
-    newSTT = '';
-    newId = '';
-    newParentId = '';
-    newIsGroup = '';
-    newName = '';
-    newItemTypeName = '';
-    newCode = '';
-    newUnit = '';
-    newItemGroupCode = '';
-    newBranchCode = '';
-    newIsActive = '';
-    newCreatedBy = '';
-    newCreatedAt = '';
-    newModifiedBy = '';
-    newModifiedAt = '';
-    new_SelectKey__wibkaw = '';
 
     constructor() { 
 
@@ -49,45 +22,6 @@ export class ProductComponent implements OnInit {
 
     ngOnInit(): void {
 
-    }
-
-    formAddProduct = '';
-
-    // Thêm sản phẩm
-    addProduct() {
-        this.listProduct.unshift({
-            Id: this.newId,
-            ParentId: this.newParentId,
-            IsGroup: this.newIsGroup,
-            Name: this.newName,
-            ItemTypeName: this.newItemTypeName,
-            Code: this.newCode,
-            Unit: this.newUnit,
-            ItemGroupCode: this.newItemGroupCode,
-            BranchCode: this.newBranchCode,
-            IsActive: this.newIsActive,
-            CreatedBy: this.newCreatedBy,
-            CreatedAt: this.newCreatedAt,
-            ModifiedBy: this.newModifiedBy,
-            ModifiedAt: this.newModifiedAt,
-            _SelectKey__wibkaw: this.new_SelectKey__wibkaw
-        });
-        // Clear form
-        this.newId = '';
-        this.newParentId = '';
-        this.newIsGroup = '';
-        this.newName = '';
-        this.newItemTypeName = '';
-        this.newCode = '';
-        this.newUnit = '';
-        this.newItemGroupCode = '';
-        this.newBranchCode = '';
-        this.newIsActive = '';
-        this.newCreatedBy = '';
-        this.newCreatedAt = '';
-        this.newModifiedBy = '';
-        this.newModifiedAt = '';
-        this.new_SelectKey__wibkaw = '';
     }
 
 }
