fd93bc955695c57b6e5895be543c2a11d21c6824
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-06 19:41:39 +0700

    update form

--- src/app/add/add.component.ts
+++ src/app/add/add.component.ts
@@ -1,15 +1,40 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
-  selector: 'app-add',
-  templateUrl: './add.component.html',
-  styleUrls: ['./add.component.css']
+	selector: 'app-add',
+	templateUrl: './add.component.html',
+	styleUrls: ['./add.component.css'],
 })
+
 export class AddComponent implements OnInit {
 
-  constructor() { }
+	addProduct = this.fb.group({
+		Id: ['', [Validators.required, Validators.minLength(5)]],
+		ParentId: ['', [Validators.required, Validators.minLength(4)]],
+		Name: ['', [Validators.required, Validators.minLength(4)]],
+		ItemTypeName: ['', Validators.required],
+		Code: ['', Validators.required],
+		Unit: ['', Validators.required],
+	});
 
-  ngOnInit(): void {
-  }
+	get Id() { return this.addProduct.get('Id')!; }
+	get ParentId() { return this.addProduct.get('ParentId')!; }
+	get Name() { return this.addProduct.get('Name')!; }
+	get ItemTypeName() { return this.addProduct.get('ItemTypeName')!; }
+	get Code() { return this.addProduct.get('Code')!; }
+	get Unit() { return this.addProduct.get('Unit')!; }
+
+  	constructor(private fb: FormBuilder) {
+
+	}
+
+	ngOnInit() {
+
+	}
+
+	onSubmit() {
+		console.warn(this.addProduct.value);
+	}
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms'
+import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -27,6 +27,7 @@ import { ToolsComponent } from './tools/tools.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
