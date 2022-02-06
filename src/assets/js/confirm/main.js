bad4899b714872c9754120b836f64362cf7dacec
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-06 16:07:41 +0700

    update confirm

---
+++ src/assets/js/confirm/main.js
@@ -0,0 +1,23 @@
+function success(status, messenger) {
+    Swal.fire(
+        status,
+        messenger,
+        'success'
+    )
+}
+
+function error(status, messenger) {
+    Swal.fire(
+        status,
+        messenger,
+        'error'
+    )
+}
+
+function warning(status, messenger) {
+    Swal.fire(
+        status,
+        messenger,
+        'warning'
+    )
+}
