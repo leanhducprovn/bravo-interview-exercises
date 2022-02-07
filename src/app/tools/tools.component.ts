64d9dd0c4418f9b60e00fcec9de93e28a93e1f4c
Lê Anh Đức <leanhduc.pro.vn@gmail.com>
2022-02-07 16:31:10 +0700

    update

--- src/app/tools/tools.component.ts
+++ src/app/tools/tools.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-tools',
-  templateUrl: './tools.component.html',
-  styleUrls: ['./tools.component.css'],
+	selector: 'app-tools',
+	templateUrl: './tools.component.html',
+	styleUrls: ['./tools.component.css'],
 })
 export class ToolsComponent implements OnInit {
 
-  clickAdd = true;
+	clickAdd = true;
 
-  constructor() {}
-
-  ngOnInit(): void {}
+	constructor() { }
+	
+	ngOnInit(): void { }
 }
